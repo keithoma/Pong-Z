60755a2ccb6b26816c5e52b5ba92036a1c19a0b5
keithoma <thomakmj@gmail.com>
2019-05-05 18:56:42 +0200

    added Bresenham's line algorithm which should work properly now
    one could optimize

--- sgfx/src/primitives.cpp
+++ sgfx/src/primitives.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+
 #include <sgfx/primitives.hpp>
 
 using namespace std;
@@ -49,26 +50,75 @@ void sgfx::fill(widget& target, rectangle rect, color::rgb_color col)
 			hline(target, {rect.top_left.x, rect.top_left.y + i}, rect.size.width, col);
 }
 
+// added some auxiliary functions, 'line_low()' and 'line_high()'; they are only used in 'line()'
+
+void line_low(sgfx::widget& target, sgfx::point p0, sgfx::point p1, sgfx::color::rgb_color col)
+{
+	int dx = p1.x - p0.x;
+	int dy = p1.y - p0.y;
+	int yi = 1;
+
+	if (dy < 0) {
+		yi = -1;
+		dy = -dy;
+	}
+
+	int big_d = 2 * dy - dx;
+	unsigned y = p0.y;
+
+	for (unsigned x = p0.x; x < p1.x; ++x) {
+    	plot(target, {x, y}, col);
+
+    	if (big_d > 0) {
+			y += yi;
+			big_d -= 2 * dx;
+		}
+
+		big_d += 2 * dy;
+	}
+}
+
+void line_high(sgfx::widget& target, sgfx::point p0, sgfx::point p1, sgfx::color::rgb_color col)
+{
+	int dx = p1.x - p0.x;
+	int dy = p1.y - p0.y;
+	int xi = 1;
+
+	if (dx < 0) {
+		xi = -1;
+		dx = -dx;
+	}
+
+	int big_d = 2 * dx - dy;
+	unsigned x = p0.x;
+
+	for (unsigned y = p0.y; y < p1.y; ++y) {
+    	plot(target, {x, y}, col);
+
+		if (big_d > 0) {
+			x += xi;
+			big_d -= 2 * dy;
+		}
+
+		big_d += 2 * dx;
+	}
+}
+
 void sgfx::line(widget& target, point p0, point p1, color::rgb_color col)
 {
-	// [Kei]: does not work propperly yet
-	const int delta_x = p0.x - p1.x;
-	const int delta_y = p0.y - p1.y;
+	// optimize first
 
-	point p = p0;
-	unsigned q = 2 * delta_x - delta_y;
-
-	const int xInc = p1.x > p0.x ? +1 : -1;
-	const int yInc = p1.y > p0.y ? +1 : -1;
-
-	while (p.x != p1.x) {
-		plot(target, p, col);
-		p.x += xInc;
-		if (q < 0)
-			q = q + 2 * delta_y;
-		else {
-			q = q + 2 * delta_y * delta_x;
-			p.y += yInc;
-		}
+    // see Bresenham's line algorithm
+	if (labs(p1.y - p0.y) < labs(p1.x - p0.x)) {
+		if (p0.x > p1.x)
+			line_low(target, {p1.x, p1.y}, {p0.x, p0.y}, col);
+		else
+			line_low(target, {p0.x, p0.y}, {p1.x, p1.y}, col);
+	}
+	else {
+		if (p0.y > p1.y)
+			line_high(target, {p1.x, p1.y}, {p0.x, p0.y}, col);
+		else
+			line_high(target, {p0.x, p0.y}, {p1.x, p1.y}, col);
 	}
 }
