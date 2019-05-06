9ffc61c2f0251c116ac552967dd49ca0649511fe
Christian Parpart <christian@parpart.family>
2019-05-06 12:30:57 +0200

    namespace usage fix & some small code refactors to make things more clear wrt. line() function

--- sgfx/src/primitives.cpp
+++ sgfx/src/primitives.cpp
@@ -5,21 +5,22 @@
 
 using namespace std;
 
-void sgfx::plot(widget& target, point p, color::rgb_color col)
-{
+namespace sgfx {
 
+void plot(widget& target, point p, color::rgb_color col)
+{
 	p.x = min(p.x, static_cast<unsigned>(target.width()) - 1);
 	p.y = min(p.y, static_cast<unsigned>(target.height()) - 1);
 
 	target.pixels()[p.y * target.width() + p.x] = col;
 }
 
-void sgfx::clear(widget& target, color::rgb_color col)
+void clear(widget& target, color::rgb_color col)
 {
 	fill(begin(target.pixels()), end(target.pixels()), col);
 }
 
-void sgfx::hline(widget& target, point p, std::uint16_t length, color::rgb_color col)
+void hline(widget& target, point p, std::uint16_t length, color::rgb_color col)
 {
 	if (p.y < target.height()) {
 		const unsigned xEnd = min(p.x + length, static_cast<unsigned>(target.width()));
@@ -30,7 +31,7 @@ void sgfx::hline(widget& target, point p, std::uint16_t length, color::rgb_color
 	}
 }
 
-void sgfx::vline(widget& target, point p, std::uint16_t length, color::rgb_color col)
+void vline(widget& target, point p, std::uint16_t length, color::rgb_color col)
 {
 	if (p.x < target.width()) {
 		const unsigned yEnd = min(p.y + length, static_cast<unsigned>(target.height()));
@@ -41,7 +42,7 @@ void sgfx::vline(widget& target, point p, std::uint16_t length, color::rgb_color
 	}
 }
 
-void sgfx::fill(widget& target, rectangle rect, color::rgb_color col)
+void fill(widget& target, rectangle rect, color::rgb_color col)
 {
 	if (rect.top_left == point{0, 0} && rect.size == dimension{target.width(), target.height()})
 		clear(target, col);
@@ -50,75 +51,73 @@ void sgfx::fill(widget& target, rectangle rect, color::rgb_color col)
 			hline(target, {rect.top_left.x, rect.top_left.y + i}, rect.size.width, col);
 }
 
-// added some auxiliary functions, 'line_low()' and 'line_high()'; they are only used in 'line()'
-
-void line_low(sgfx::widget& target, sgfx::point p0, sgfx::point p1, sgfx::color::rgb_color col)
+void line(widget& target, point p0, point p1, color::rgb_color col)
 {
-	int dx = p1.x - p0.x;
-	int dy = p1.y - p0.y;
-	int yi = 1;
+	auto line_low = [](widget& target, point p0, point p1, color::rgb_color col) {
+		int dx = p1.x - p0.x;
+		int dy = p1.y - p0.y;
+		int yi = 1;
 
-	if (dy < 0) {
-		yi = -1;
-		dy = -dy;
-	}
-
-	int big_d = 2 * dy - dx;
-	unsigned y = p0.y;
-
-	for (unsigned x = p0.x; x < p1.x; ++x) {
-    	plot(target, {x, y}, col);
-
-    	if (big_d > 0) {
-			y += yi;
-			big_d -= 2 * dx;
+		if (dy < 0) {
+			yi = -1;
+			dy = -dy;
 		}
 
-		big_d += 2 * dy;
-	}
-}
+		int big_d = 2 * dy - dx;
+		unsigned y = p0.y;
 
-void line_high(sgfx::widget& target, sgfx::point p0, sgfx::point p1, sgfx::color::rgb_color col)
-{
-	int dx = p1.x - p0.x;
-	int dy = p1.y - p0.y;
-	int xi = 1;
+		for (unsigned x = p0.x; x < p1.x; ++x) {
+			plot(target, {x, y}, col);
 
-	if (dx < 0) {
-		xi = -1;
-		dx = -dx;
-	}
+			if (big_d > 0) {
+				y += yi;
+				big_d -= 2 * dx;
+			}
 
-	int big_d = 2 * dx - dy;
-	unsigned x = p0.x;
+			big_d += 2 * dy;
+		}
+	};
 
-	for (unsigned y = p0.y; y < p1.y; ++y) {
-    	plot(target, {x, y}, col);
+	static auto line_high = [](widget& target, point p0, point p1, color::rgb_color col) {
+		int dx = p1.x - p0.x;
+		int dy = p1.y - p0.y;
+		int xi = 1;
 
-		if (big_d > 0) {
-			x += xi;
-			big_d -= 2 * dy;
+		if (dx < 0) {
+			xi = -1;
+			dx = -dx;
 		}
 
-		big_d += 2 * dx;
-	}
-}
+		int big_d = 2 * dx - dy;
+		unsigned x = p0.x;
 
-void sgfx::line(widget& target, point p0, point p1, color::rgb_color col)
-{
+		for (unsigned y = p0.y; y < p1.y; ++y) {
+			plot(target, {x, y}, col);
+
+			if (big_d > 0) {
+				x += xi;
+				big_d -= 2 * dy;
+			}
+
+			big_d += 2 * dx;
+		}
+	};
+
 	// optimize first
 
-    // see Bresenham's line algorithm
+	// see Bresenham's line algorithm
 	if (labs(p1.y - p0.y) < labs(p1.x - p0.x)) {
 		if (p0.x > p1.x)
-			line_low(target, {p1.x, p1.y}, {p0.x, p0.y}, col);
+			line_low(target, p1, p0, col);
 		else
-			line_low(target, {p0.x, p0.y}, {p1.x, p1.y}, col);
+			line_low(target, p0, p1, col);
 	}
 	else {
 		if (p0.y > p1.y)
-			line_high(target, {p1.x, p1.y}, {p0.x, p0.y}, col);
+			line_high(target, p1, p0, col);
 		else
-			line_high(target, {p0.x, p0.y}, {p1.x, p1.y}, col);
+			line_high(target, p0, p1, col);
 	}
 }
+
+}  // namespace sgfx
