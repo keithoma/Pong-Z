b27af2a716404d56dd801f70895caef9461091e2
Christian Parpart <christian@parpart.family>
2019-05-08 18:07:18 +0200

    breseham for fun and points

--- sgfx/src/primitives.cpp
+++ sgfx/src/primitives.cpp
@@ -1,17 +1,20 @@
+#include <sgfx/primitives.hpp>
+
 #include <algorithm>
 #include <iostream>
 #include <tuple>
+#include <functional>
+#include <cassert>
 
-#include <sgfx/primitives.hpp>
-
+using namespace std::placeholders;
 using namespace std;
 
 namespace sgfx {
 
 void plot(widget& target, point p, color::rgb_color col)
 {
-	p.x = min(p.x, static_cast<unsigned>(target.width()) - 1);
-	p.y = min(p.y, static_cast<unsigned>(target.height()) - 1);
+	p.x = min(p.x, static_cast<int>(target.width()) - 1);
+	p.y = min(p.y, static_cast<int>(target.height()) - 1);
 
 	target.pixels()[p.y * target.width() + p.x] = col;
 }
@@ -24,7 +27,7 @@ void clear(widget& target, color::rgb_color col)
 void hline(widget& target, point p, std::uint16_t length, color::rgb_color col)
 {
 	if (p.y < target.height()) {
-		const unsigned xEnd = min(p.x + length, static_cast<unsigned>(target.width()));
+		const unsigned xEnd = min(p.x + length, static_cast<int>(target.width()));
 		while (p.x < xEnd) {
 			plot(target, p, col);
 			++p.x;
@@ -35,7 +38,7 @@ void hline(widget& target, point p, std::uint16_t length, color::rgb_color col)
 void vline(widget& target, point p, std::uint16_t length, color::rgb_color col)
 {
 	if (p.x < target.width()) {
-		const unsigned yEnd = min(p.y + length, static_cast<unsigned>(target.height()));
+		const unsigned yEnd = min(p.y + length, static_cast<int>(target.height()));
 		while (p.y < yEnd) {
 			plot(target, p, col);
 			++p.y;
@@ -52,58 +55,48 @@ void fill(widget& target, rectangle rect, color::rgb_color col)
 			hline(target, {rect.top_left.x, rect.top_left.y + i}, rect.size.width, col);
 }
 
+/**
+ * Breseham Algorithm line point generator.
+ *
+ * @param A indexed access to point coordinate, where 0 means X and 1 means Y.
+ * @param B indexed access to point coordinate, where 0 means X and 1 means Y.
+ *
+ * @param p0 first point
+ * @param p1 second point
+ * @param drawer function to invoke for each generated point.
+ */
+template <const size_t A, const size_t B>
+constexpr void breseham(point p0, point p1, std::function<void(point const&)> sink) {
+	static_assert(A == 0 || A == 1, "A must be 0 (for X) or 1 (for Y).");
+	static_assert(B == 0 || B == 1, "B must be 0 (for X) or 1 (for Y).");
+	static_assert(A != B, "A must not be equal to B.");
+	assert(p0.y <= p1.y, "Must draw from top to bottom (p0.y <= p1.y).");
+
+	// compute delta x/y and the increment value for the walking coordinate
+	auto const [delta, increment] = [](auto p0, auto p1) {
+		if (const auto delta = p1 - p0; get<B>(delta) >= 0)
+			return make_tuple(delta, +1);
+		else {
+			const auto sgn = int{A == 0 /*A is X*/ ? +1 : -1};
+			return make_tuple(point{sgn * delta.x, -sgn * delta.y}, -1);
+		}
+	}(p0, p1);
+
+	auto big_d = int{2 * get<B>(delta) - get<A>(delta)};
+	auto p = p0;
+	while (get<A>(p) <= get<A>(p1)) {
+		sink(p);
+		if (big_d > 0) {
+			big_d -= 2 * get<A>(delta);
+			get<B>(p) += increment;
+		}
+		++get<A>(p);
+		big_d += 2 * get<B>(delta);
+	}
+};
+
 void line(widget& target, point p0, point p1, color::rgb_color col)
 {
-	auto const line_low = [](widget& target, point p0, point p1, color::rgb_color col) {
-		const auto [dx, dy, yi] = [p0, p1]() {
-			const int dx = p1.x - p0.x;
-			const int dy = p1.y - p0.y;
-			if (dy < 0)
-				return make_tuple(+dx, -dy, -1);
-			else
-				return make_tuple(+dx, +dy, +1);
-		}();
-
-		int big_d = 2 * dy - dx;
-		unsigned y = p0.y;
-
-		for (unsigned x = p0.x; x < p1.x; ++x) {
-			plot(target, {x, y}, col);
-
-			if (big_d > 0) {
-				y += yi;
-				big_d -= 2 * dx;
-			}
-
-			big_d += 2 * dy;
-		}
-	};
-
-	static const auto line_high = [](widget& target, point p0, point p1, color::rgb_color col) {
-		const auto [dx, dy, xi] = [p0, p1]() {
-			const int dx = p1.x - p0.x;
-			const int dy = p1.y - p0.y;
-			if (dx < 0)
-				return make_tuple(-dx, +dy, -1);
-			else
-				return make_tuple(+dx, +dy, +1);
-		}();
-
-		int big_d = 2 * dx - dy;
-		unsigned x = p0.x;
-
-		for (unsigned y = p0.y; y < p1.y; ++y) {
-			plot(target, {x, y}, col);
-
-			if (big_d > 0) {
-				x += xi;
-				big_d -= 2 * dy;
-			}
-
-			big_d += 2 * dx;
-		}
-	};
-
 	if (p0 == p1) {
 		// point
 		plot(target, p0, col);
@@ -119,16 +112,16 @@ void line(widget& target, point p0, point p1, color::rgb_color col)
 	else if (labs(p1.y - p0.y) < labs(p1.x - p0.x)) {
 		// non-trivial line: with the power of Bresenham
 		if (p0.x > p1.x)
-			line_low(target, p1, p0, col);
+			breseham<0, 1>(p1, p0, bind(plot, ref(target), _1, col));
 		else
-			line_low(target, p0, p1, col);
+			breseham<0, 1>(p0, p1, bind(plot, ref(target), _1, col));
 	}
 	else {
 		// non-trivial line: with the power of Bresenham
 		if (p0.y > p1.y)
-			line_high(target, p1, p0, col);
+			breseham<1, 0>(p1, p0, bind(plot, ref(target), _1, col));
 		else
-			line_high(target, p0, p1, col);
+			breseham<1, 0>(p0, p1, bind(plot, ref(target), _1, col));
 	}
 }
 
