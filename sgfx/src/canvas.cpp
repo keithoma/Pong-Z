9b264944377fdcd1a392270abf6425660aec1371
Christian Parpart <christian@parpart.family>
2019-05-09 17:31:10 +0200

    another fix for when drawing beyond target's borders.

--- sgfx/src/canvas.cpp
+++ sgfx/src/canvas.cpp
@@ -12,9 +12,11 @@ canvas canvas::colored(dimension size, color::rgb_color col)
 
 void draw(widget& target, const canvas& source, point top_left)
 {
+	auto const static inRange = [](auto x, auto begin, auto end) { return begin <= x && x < end; };
+
 	for (int x = 0; x < source.width(); ++x)
 		for (int y = 0; y < source.height(); ++y)
-			if (top_left.x + x < target.width() && top_left.y + y < target.height())
+			if (inRange(top_left.x + x, 0, target.width()) && inRange(top_left.y + y, 0, target.height()))
 				target[top_left + point{x, y}] = source[point{x, y}];
 
 	// TODO: super naive implementation wants to be replaced with its superior
