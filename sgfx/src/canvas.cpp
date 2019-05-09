5a43fe19af01fb7caf60e396ef0a18740fd6f10f
Christian Parpart <christian@parpart.family>
2019-05-09 17:17:18 +0200

    bug-fix in sgfx::draw ;-(

--- sgfx/src/canvas.cpp
+++ sgfx/src/canvas.cpp
@@ -14,7 +14,7 @@ void draw(widget& target, const canvas& source, point top_left)
 {
 	for (int x = 0; x < source.width(); ++x)
 		for (int y = 0; y < source.height(); ++y)
-			if (top_left.x + x < source.width() && top_left.y + y < source.height())
+			if (top_left.x + x < target.width() && top_left.y + y < target.height())
 				target[top_left + point{x, y}] = source[point{x, y}];
 
 	// TODO: super naive implementation wants to be replaced with its superior
